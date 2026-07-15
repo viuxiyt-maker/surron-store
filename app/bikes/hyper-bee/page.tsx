2bc3d51ae878f6bca7746352813b352c61a6a91b
viuxiyt-maker <viuxiyt@gmail.com>
2026-07-16 01:00:38 +0200

    Fix hyper bee page export

--- app/bikes/hyper-bee/page.tsx
+++ app/bikes/hyper-bee/page.tsx
@@ -1,7 +1,7 @@
 export default function HyperBeePage() {
   return (
-    <main className="min-h-screen bg-black text-white">
-      <h1 className="text-5xl font-bold">
+    <main className="min-h-screen bg-[#050507] text-white">
+      <h1 className="text-5xl font-black">
         Surron Hyper Bee
       </h1>
     </main>
