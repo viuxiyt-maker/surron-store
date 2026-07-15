96ca4d986bf0254434994b2ae67a0293a190a102
viuxiyt-maker <viuxiyt@gmail.com>
2026-07-16 01:03:17 +0200

    Fix ultra bee page

---
+++ app/bikes/ultra-bee/page.tsx
@@ -0,0 +1,9 @@
+export default function UltraBeePage() {
+  return (
+    <main className="min-h-screen bg-[#050507] text-white flex items-center justify-center">
+      <h1 className="text-5xl font-black">
+        Surron Ultra Bee
+      </h1>
+    </main>
+  );
+}
