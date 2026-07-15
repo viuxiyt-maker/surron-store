b24f5dbccc3a51ccff7d4b9cfecd6c8791880a13
viuxiyt-maker <viuxiyt@gmail.com>
2026-07-16 00:59:15 +0200

    Fix hyper bee page

---
+++ app/bikes/hyper-bee/page.tsx
@@ -0,0 +1,9 @@
+export default function HyperBeePage() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        Surron Hyper Bee
+      </h1>
+    </main>
+  );
+}
